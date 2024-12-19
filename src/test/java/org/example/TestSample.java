c4673084dcdcc2078513333530306d89fc577f45
matthcol <matthias@learning64.net>
2024-12-19 08:59:38 +0100

    first commit

---
+++ src/test/java/org/example/TestSample.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSample {
+
+    @Test
+    void test_dummy(){
+        fail();
+    }
+}

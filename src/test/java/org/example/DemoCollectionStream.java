914a7f1ac58ac3e1a2cd772198cba0b4c8c216b3
matthcol <matthias@learning64.net>
2024-12-19 14:37:56 +0100

    more collect and stream

--- src/test/java/org/example/DemoCollectionStream.java
+++ src/test/java/org/example/DemoCollectionStream.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DemoCollectionStream {
@@ -90,9 +89,10 @@ public class DemoCollectionStream {
     void demo6existingFunctions(){
         cities.stream()
                 .map(MiscTools::encodeCity) // ref static method
-                .forEach(city -> System.out.println(city));
+                .forEach(System.out::println); // ref instance method
     }
 
+    // list of all collectors: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/util/stream/Collectors.html
 
     @Test
     void demo7collect(){
@@ -101,6 +101,7 @@ public class DemoCollectionStream {
                 .map(city -> city.toUpperCase())
                 .toList(); // collect (shortcut): since Java 16
         System.out.println(listCityFilteredU);
+        System.out.println(listCityFilteredU.getClass());
     }
 
     @Test
@@ -110,6 +111,110 @@ public class DemoCollectionStream {
                 .map(city -> city.toUpperCase())
                 .collect(Collectors.toList()); // Java 8+
         System.out.println(listCityFilteredU);
+        System.out.println(listCityFilteredU.getClass());
+    }
+
+    @Test
+    void demo9collect(){
+        var listCityFilteredU = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(city -> city.toUpperCase())
+                .collect(Collectors.toCollection(() -> new ArrayList<>(1000)));
+        System.out.println(listCityFilteredU);
+        listCityFilteredU.add("MARSEILLE");
+        System.out.println(listCityFilteredU);
+        System.out.println(listCityFilteredU.getClass());
+    }
+
+    @Test
+    void demo10collect(){
+        // NB:
+        //      String::toUpperCase has 2 type profiles:
+        //          * String -> String (version without parameter)
+        //          * String x Locale -> String (version with parameter of type Locale)
+        //      ArrayList::new has 3 type profiles:
+        //          * () -> ArrayList<E>
+        //          * int -> ArrayList<E>
+        //          * Collection<? extends E> -> ArrayList<E>
+        List<String> listCityFilteredU = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(String::toUpperCase) // ref object method (choose version without parameter among 2)
+                .collect(Collectors.toCollection(ArrayList::new)); // ref constructor (choose version without parameter among 3)
+        System.out.println(listCityFilteredU);
+        listCityFilteredU.add("MARSEILLE");
+        System.out.println(listCityFilteredU);
+        System.out.println(listCityFilteredU.getClass());
+    }
+
+    // Exercise: idem with collect into a Set, SortedSet, Deque
+
+    @Test
+    void demo11collect() {
+        Set<String> filteredCities = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(String::toUpperCase)
+                .collect(Collectors.toSet());
+        System.out.println(filteredCities);
+        System.out.println(filteredCities.getClass());
+    }
+
+    @Test
+    void demo12collect() {
+        Set<String> filteredCities = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(String::toUpperCase)
+                .collect(Collectors.toCollection(HashSet::new));
+        System.out.println(filteredCities);
+        Collections.addAll(filteredCities, "PAU", "MARSEILLE");
+        System.out.println(filteredCities);
+        System.out.println(filteredCities.getClass());
+    }
+
+    @Test
+    void demo13collect() {
+        NavigableSet<String> filteredCities = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(String::toUpperCase)
+                .collect(Collectors.toCollection(TreeSet::new));
+        System.out.println(filteredCities);
+        Collections.addAll(filteredCities, "ZEGERSCAPPEL", "PAU", "MARSEILLE", "AVIGNON");
+        System.out.println(filteredCities);
+        System.out.println(filteredCities.getClass());
+        System.out.println(filteredCities.ceiling("Z")); // ZEGERSCAPPEL
+        System.out.println(filteredCities.floor("Z")); // PAU
+    }
+
+    @Test
+    void demo14collect() {
+        Deque<String> filteredCities = cities.stream()
+                .filter(city -> city.length() < 6)
+                .map(String::toUpperCase)
+                .collect(Collectors.toCollection(ArrayDeque::new));
+        System.out.println(filteredCities);
+        Collections.addAll(filteredCities, "ZEGERSCAPPEL", "PAU", "MARSEILLE", "AVIGNON");
+        System.out.println(filteredCities);
+        filteredCities.addFirst("BAYONNE");
+        System.out.println(filteredCities);
+        System.out.println("pop first city: " + filteredCities.removeFirst());
+        System.out.println(filteredCities);
+        System.out.println(filteredCities.getClass());
+    }
+
+    @Test
+    void demoShortcutForEach(){
+        // "for each" loop of Java 5
+        for (String city: cities) {
+            System.out.println(city);
+        }
+        // with var
+        for (var city: cities) {
+            System.out.println(city);
+        }
+        // stream forEach
+        cities.stream()
+                .forEach(System.out::println);
+        // stream forEach shortcut for collections
+        cities.forEach(System.out::println);
     }
 }
 
